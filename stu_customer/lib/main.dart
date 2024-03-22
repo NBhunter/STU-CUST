7802c7b9c3d0cd2662edd98d5bd118bd02a3b35c
NBhunter <bangnguyen01072001@gmail.com>
2024-03-22 14:36:28 +0700

    getlocation, direct route in map

--- stu_customer/lib/main.dart
+++ stu_customer/lib/main.dart
@@ -4,9 +4,16 @@ import 'package:stu_customer/app/splash_screen/splash_screen.dart';
 import 'package:stu_customer/controller/NavigatorController.dart';
 import 'package:stu_customer/screen/home.dart';
 import 'package:stu_customer/screen/layout.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:stu_customer/firebase_options.dart';
 
 const kWebRecaptchaSiteKey = '6Lemcn0dAAAAABLkf6aiiHvpGD6x-zF3nOSDU2M8';
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => NavigatorController(),
---
+++ stu_customer/lib/screen/Map/simplemap.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+void main() {
+  runApp(
+      const MaterialApp(debugShowCheckedModeBanner: false, home: SimpleMap()));
+}
+
+class SimpleMap extends StatefulWidget {
+  const SimpleMap({super.key});
+
+  @override
+  State createState() => FullMapState();
+}
+
+class FullMapState extends State<SimpleMap> {
+  MapboxMap? mapboxMap;
+
+  _onMapCreated(MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Goong Map'),
+        ),
+        body: Stack(children: [
+          SizedBox(
+            child: MapWidget(
+              key: const ValueKey("mapWidget"),
+              resourceOptions: ResourceOptions(
+                  accessToken: "XMtOx7K0p4mqz8T6zK5EsMj6yQ3Fu1NlSdobLX7R"),
+              cameraOptions: CameraOptions(
+                  center: Point(coordinates: Position(1, 1)).toJson(),
+                  zoom: 3.0),
+              styleUri: MapboxStyles.DARK,
+              textureView: true,
+              onMapCreated: _onMapCreated,
+            ),
+          ),
+        ]));
+  }
+}
